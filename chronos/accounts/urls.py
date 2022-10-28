9fabe279019ca1e36c5bc681e648906c4808769c
erinbedri <erinbedri@gmail.com>
2022-10-28 12:15:10 +0200

    fixed account url names

--- chronos/accounts/urls.py
+++ chronos/accounts/urls.py
@@ -4,12 +4,12 @@ from chronos.accounts import views
 
 
 urlpatterns = [
-    path('register/', views.register_account, name='register'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('show/', views.show_account, name='show'),
-    path('edit/', views.edit_account, name='edit'),
-    path('delete/', views.delete_account, name='delete'),
+    path('register/', views.register_account, name='register_account'),
+    path('login/', views.login_user, name='login_account'),
+    path('logout/', views.logout_user, name='logout_account'),
+    path('show/', views.show_account, name='show_account'),
+    path('edit/', views.edit_account, name='edit_account'),
+    path('delete/', views.delete_account, name='delete_account'),
 
     # path('register/', RegisterView.as_view(), name='register profile'),
 ]
