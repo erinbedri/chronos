f06b3a05669342c90245f5a0bb19df914ae44e4a
erinbedri <erinbedri@gmail.com>
2022-10-28 16:29:59 +0200

    fixed url for 403, 404, 500 templates

--- chronos/accounts/views.py
+++ chronos/accounts/views.py
@@ -102,7 +102,7 @@ def edit_account(request):
         if form.is_valid():
             form.save()
             messages.success(request, PROFILE_EDIT_SUCCESS_MESSAGE)
-            return redirect('accounts:show')
+            return redirect('accounts:show_account')
         else:
             messages.error(request, form.errors)
     else:
